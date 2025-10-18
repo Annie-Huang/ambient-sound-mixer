2afc15b9a07cc62d108323efcd1faad4865a0a07
Annie Huang <annie.hn@gmail.com>
2025-10-18 16:21:02 +1100

    In loadSound method in soundManager.js, setup the audio first.

--- js/soundManager.js
+++ js/soundManager.js
@@ -7,7 +7,20 @@ export class SoundManager {
 
   // Load a sound file
   loadSound(soundId, filePath) {
-    console.log(`Loading sound ${soundId} from ${filePath}`);
-    return true;
+    // console.log(`Loading sound ${soundId} from ${filePath}`);
+    // return true;
+
+    try {
+      const audio = new Audio();
+      audio.src = filePath;
+      audio.loop = true;
+
+      // if you do 'auto', you will need to make sure it preloads all the audio before anything.
+      // But in mobile, it doesn't work because it takes longer to load, so we load the metadata first.
+      audio.preload = 'metadata';
+    } catch (error) {
+      console.error(`Failed to load sound ${error}`);
+      return false;
+    }
   }
 }
