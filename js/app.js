f5bab93cb5bf11b85a46fff6a23186e9bc73e999
Annie Huang <annie.hn@gmail.com>
2025-10-18 16:14:55 +1100

    Use SoundManager in the app.js and call loadSound in init()

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 // !!!! very important, all import needs to have .js in there, otherwise you will see error:
 // app.js:2  GET http://localhost:3000/js/soundData net::ERR_ABORTED 404 (Not Found)
 import { sounds, defaultPresets } from './soundData.js';
+import { SoundManager } from './soundManager.js';
 
 class AmbientMixer {
   // Initialize dependencies and default state
   constructor() {
-    console.log('Initializing state...');
-    this.soundManager = null;
+    // console.log('Initializing state...');
+    this.soundManager = new SoundManager();
     this.ui = null;
     this.presetManager = null;
     this.timer = null;
@@ -16,7 +17,8 @@ class AmbientMixer {
 
   init() {
     try {
-      console.log('Initializing app...');
+      // console.log('Initializing app...');
+      this.soundManager.loadSound('rain', 'audio/rain.mp3');
       this.isInitialized = true;
     } catch (error) {
       console.error('Failed to initialze app: ', error);
