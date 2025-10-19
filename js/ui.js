73305adda93303a7ddfb8fba9e6b1475aa69be5e
Annie Huang <annie.hn@gmail.com>
2025-10-19 15:59:23 +1100

    Get the corrrect element in the init function

--- js/ui.js
+++ js/ui.js
@@ -11,4 +11,17 @@ export class UI {
     this.timeSelect = null;
     this.themeToggle = null;
   }
+
+  init() {
+    this.soundCardsContainer = document.querySelector('.grid');
+    this.masterVolumeSlider = document.getElementById('masterVolume');
+    this.masterVolumeValue = document.getElementById('masterVolumeValue');
+    this.playPauseButton = document.getElementById('playPauseAll');
+    this.resetButton = document.getElementById('resetAll');
+    this.modal = document.getElementById('savePresetModal');
+    this.customPresetsContainer = document.getElementById('customPresets');
+    this.timerDisplay = document.getElementById('timerDisplay');
+    this.timeSelect = document.getElementById('timerSelect');
+    this.themeToggle = document.getElementById('themeToggle');
+  }
 }
