7bd8ff2950b435404df75c4f5f064cc3c44475a0
Annie Huang <annie.hn@gmail.com>
2025-10-19 15:54:06 +1100

    Create constructor in the UI class

---
+++ js/ui.js
@@ -0,0 +1,14 @@
+export class UI {
+  constructor() {
+    this.soundCardsContainer = null;
+    this.masterVolumeSlider = null;
+    this.masterVolumeValue = null;
+    this.playPauseButton = null;
+    this.resetButton = null;
+    this.modal = null;
+    this.customPresetsContainer = null;
+    this.timerDisplay = null;
+    this.timeSelect = null;
+    this.themeToggle = null;
+  }
+}
